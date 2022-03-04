dba5c44d308e66c3cfc831ebdaac898d3513e3ce
andres holivin <andres12holivin@gmail.com>
2022-03-05 00:31:30 +0700

    modif schedule

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     #     msg.send("this schedule run every minutes on " + str(datetime.now()))
 
 
-    @sched.scheduled_job('cron', day_of_week='0-6', hour=0, minute=28)
+    @sched.scheduled_job('cron', day_of_week='0-6', hour=17, minute=35)
     def scheduled_job():
         msg = PushMessageTelegram()
         msg.send("this schedule run every day on " + str(datetime.now()))
