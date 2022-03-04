bc547273b1d916328f4e26b47e4916b4e28e74a4
andres holivin <andres12holivin@gmail.com>
2022-03-04 16:01:46 +0700

    modif

--- app.py
+++ app.py
@@ -70,17 +70,17 @@ def job():
     print("I'm working...")
 
 
-sched = BlockingScheduler()
+# sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
-
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+# @sched.scheduled_job('interval', minutes=3)
+# def timed_job():
+#     print('This job is run every three minutes.')
+#
+#
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+# def scheduled_job():
+#     print('This job is run every weekday at 5pm.')
 
 
 def cronjob():
@@ -93,10 +93,10 @@ def cronjob():
 
 
 if __name__ == "__main__":
-    # scheduler = BlockingScheduler()
-    # scheduler.add_job(cronjob, "interval", seconds=30)
-    #
-    # scheduler.start()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(cronjob, "interval", seconds=30)
+
+    scheduler.start()
     # sched.start()
 
     # schedule \
@@ -108,5 +108,5 @@ if __name__ == "__main__":
     # hcSchedule.run_schedule_clock_in()
     # hc = HcBinus()
     # hc.clock_in()
-    msg = PushMessageTelegram()
-    msg.send("test")
+    # msg = PushMessageTelegram()
+    # msg.send("test")
