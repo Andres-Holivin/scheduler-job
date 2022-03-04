59f668251670540267bc1b1868b25d39285ab7b8
andres holivin <andres12holivin@gmail.com>
2022-03-04 16:14:24 +0700

    modif schedule

--- app.py
+++ app.py
@@ -84,12 +84,10 @@ def job():
 
 
 def cronjob():
-    """
-    Main cron job.
-    The main cronjob to be run continuously.
-    """
     print("Cron job is running")
     print("Tick! The time is: %s" % datetime.now())
+    msg = PushMessageTelegram()
+    msg.send("Run schedule on The time is: %s" % datetime.now())
 
 
 if __name__ == "__main__":
